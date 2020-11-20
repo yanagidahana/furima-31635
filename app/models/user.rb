baeed47a04bcd4359e79dbffe7f77f021d5e9952
yanagidahana <hana878787zzz@gmail.com>
2020-11-20 15:42:42 +0900

    モデルの単体テストを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
          validates :first_name_kana, presence: true
          validates :birthday , presence: true
 
-         validates :nickname, presence: true, length: { maximum: 6 }
 
 
 
