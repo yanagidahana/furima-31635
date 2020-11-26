85fc2bbf24a64469c474dd68c371a551577bfaae
yanagidahana <hana878787zzz@gmail.com>
2020-11-26 17:08:41 +0900

    Formオブジェクトを作成する

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,2 +1,13 @@
-class Buy < ApplicationRecord
+class Buy
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone, :record
+
+  # ここにバリデーションの処理を書く
+  validates :postcode, :prefecture_id, :city, :block, :phone,  presence: true
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+    record = Records.create(user: user, item: item)
+    areas.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone: phone, record: record, record_id: record.id)
+  end
 end
