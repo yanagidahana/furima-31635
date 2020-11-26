154f79a6e75bc922329796a1ea0e74e1a38dbdf4
yanagidahana <hana878787zzz@gmail.com>
2020-11-26 11:32:14 +0900

    destroyアクションのルーティングを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,9 @@ class ItemsController < ApplicationController
     end  
   end  
 
+  def destroy
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :category_id, :status_id, :shipping_id, :shipping_place_id, :date_shipment_id, :explanation, :price, :image).merge(user_id: current_user.id)
