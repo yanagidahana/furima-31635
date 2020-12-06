375debfa94904f76b6ef097152b25fe34770bda9
yanagidahana <hana878787zzz@gmail.com>
2020-12-06 22:14:14 +0900

    comments_controller.rbを編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,8 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(text: params[:comment][:text])
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment
+    end
   end
 end  
